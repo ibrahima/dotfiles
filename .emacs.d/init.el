caddcff5587d03eeed0b6d1978201b6eeb018762
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2012-10-19 16:14:40 -0700

    CEDET stuff.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,5 @@
+(load-file "~/.emacs.d/cedet-1.1/common/cedet.el")
+
 (require 'package)
 (package-initialize)
 
@@ -39,3 +41,33 @@
 ;; User-setting area is below this line.
 
 (setq backup-directory-alist `(("." . "~/.emacs.d/saves")))
+
+(global-ede-mode t)
+
+; Semantic stuff
+(semantic-load-enable-excessive-code-helpers)
+(require 'semantic-ia)
+(require 'semantic-gcc)
+(semantic-add-system-include "/usr/include" 'c++-mode)
+
+(ede-cpp-root-project "Bulletsim"
+                :name "Bulletsim"
+                :file "~/research/bulletsim/CMakeLists.txt"
+                :include-path '("/"
+                                "/src"
+                               )
+                :system-include-path '("/opt/gurobi501/linux64/include")
+                :spp-table '(("isUnix" . "")
+                             ("BOOST_TEST_DYN_LINK" . "")))
+
+(global-unset-key (kbd "<mouse-2>"))
+
+(defun my-cedet-hook ()
+  (local-set-key [(control return)] 'semantic-ia-complete-symbol)
+  (local-set-key "\C-c?" 'semantic-ia-complete-symbol-menu)
+  (local-set-key "\C-c>" 'semantic-complete-analyze-inline)
+  (local-set-key "\C-cp" 'semantic-analyze-proto-impl-toggle)
+  (local-set-key (kbd "<mouse-2>") 'semantic-ia-fast-jump))
+(add-hook 'c-mode-common-hook 'my-cedet-hook)
+
+(server-start)
