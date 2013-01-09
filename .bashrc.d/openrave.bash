8bd468d1aace478925b83a57d80d92d0a23786e3
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2013-01-08 21:29:14 -0800

    Some temporary environment stuff for research stuff.

--- .bashrc.d/openrave.bash
+++ .bashrc.d/openrave.bash
@@ -1,2 +1,7 @@
 source /usr/share/openrave-0.8/openrave.bash
 source /usr/share/openrave-0.8/openrave_completion.bash
+
+# For source installed OpenRAVE
+export LD_LIBRARY_PATH=/usr/local/lib
+
+export PYTHONPATH=~/research/trajopt/src/trajopt/py:~/research/trajopt/build/lib:$PYTHONPATH
