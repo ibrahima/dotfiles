83d6c439e14bba9584fe99e01b0fbd9d46e7b228
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2013-03-23 19:21:54 -0700

    Add proper MobileOrg configuration, including auto-sync.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,6 +8,7 @@
 
 (require 'ido)
 (ido-mode t)
+(ido-everywhere)
 (setq ido-enable-flex-matching t)
 
 ; Desktop mode saves your emacs session and restores it when you start emacs
@@ -99,6 +100,7 @@
 (setq auto-save-file-name-transforms `((".*" "~/.emacs.d/saves" t)))
 
 ;; Org mode
+(setq org-directory "~/SparkleShare/braindump/")
 (setq org-agenda-files '("~/SparkleShare/braindump"
 						 ))
 (setq org-todo-keywords
@@ -161,7 +163,10 @@
 (setq mouse-autoselect-window t)
 
 ; MobileOrg
-(setq org-mobile-directory "~/Dropbox/Org")
+(if (eq system-type 'windows-nt)
+    (setq org-mobile-directory "f:/Dropbox/MobileOrg")
+  (setq org-mobile-directory "~/Dropbox/MobileOrg"))
+(setq org-mobile-inbox-for-pull (concat (file-name-as-directory org-mobile-directory) "from-mobile.org"))
 
 ; OpenRAVE include paths, can't believe I didn't know about this though it was right on the site
 (defun openrave-package-path ()
@@ -204,3 +209,37 @@
 (global-set-key (kbd "S-C-<down>") 'shrink-window)
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)
 
+(defun org-mobile-push-with-delay (secs)
+  (when org-mobile-push-timer
+    (cancel-timer org-mobile-push-timer))
+  (setq org-mobile-push-timer
+        (run-with-idle-timer
+         (* 1 secs) nil 'org-mobile-push)))
+
+(add-hook 'after-save-hook
+ (lambda ()
+   (when (eq major-mode 'org-mode)
+     (dolist (file (org-mobile-files-alist))
+       (if (string= (expand-file-name (car file)) (buffer-file-name))
+           (org-mobile-push-with-delay 30)))
+   )))
+
+(run-at-time "00:05" 86400 '(lambda () (org-mobile-push-with-delay 1))) ;; refreshes agenda file each day
+
+(defun install-monitor (file secs)
+  (run-with-timer
+   0 secs
+   (lambda (f p)
+     (unless (< p (second (time-since (elt (file-attributes f) 5))))
+       (org-mobile-pull)))
+   file secs))
+
+(install-monitor (file-truename
+                  (concat
+                   (file-name-as-directory org-mobile-directory)
+                          "from-mobile.org"))
+                 5)
+
+;; Do a pull every 5 minutes to circumvent problems with timestamping
+;; (ie. dropbox bugs)
+(run-with-timer 0 (* 5 60) 'org-mobile-pull)
