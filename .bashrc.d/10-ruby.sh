bc8d594402d0992a2698b30e2218c5e9322e968e
Ibrahim Awwal <ibrahim@rhino.ibrahimawwal.net>
2013-07-16 02:56:43 +0000

    Enable rbenv

--- .bashrc.d/10-ruby.sh
+++ .bashrc.d/10-ruby.sh
@@ -1,5 +1,10 @@
-source ~/.rvm/scripts/rvm
+if [ -d ~/.rvm ]; then
+    source ~/.rvm/scripts/rvm
+fi
 
-PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+export PATH="$HOME/.rbenv/bin:$PATH"
+
+eval "$(rbenv init -)"
 
 export RUBY_FFI_NCURSES_LIB=/lib/x86_64-linux-gnu/libncursesw.so.5
