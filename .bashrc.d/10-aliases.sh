c95753a458961ff157fcf60c4cad2415e203667c
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2013-09-09 20:09:12 -0700

    Change ge alias to use nohup so you don't need to disown the process.

--- .bashrc.d/10-aliases.sh
+++ .bashrc.d/10-aliases.sh
@@ -33,7 +33,7 @@ alias gitcompare='git log --left-right --graph --cherry-pick --oneline'
 
 alias emacs='emacsclient -a "" -t'
 alias e='emacsclient -a "" -t'
-alias ge='emacsclient -c -a "" &'
+alias ge='nohup emacsclient -c -a "" > ~/tmp/emacs.log &'
 export VISUAL='emacsclient -a "" -t'
 export EDITOR='emacsclient -a "" -t'
 
