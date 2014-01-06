d57a1c869bd1b37b07f7ccc8bfc488839f9619a7
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-01-05 18:05:54 -0800

    Change android SDK path

--- .bashrc.d/10-android.sh
+++ .bashrc.d/10-android.sh
@@ -1,5 +1,5 @@
-export PATH=~/android/android-sdk-linux/platform-tools:~/android/android-sdk-linux/tools:$PATH
-export ANDROID_HOME=~/android/android-sdk-linux
+export PATH=~/android/sdk/platform-tools:~/android/sdk/tools:$PATH
+export ANDROID_HOME=~/android/sdk
 
 # Use ccache with the Android build system
 export USE_CCACHE=1
