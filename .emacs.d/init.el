c2c468a6c96d4879c58b124879caf60303a2ba87
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-01-09 13:32:06 -0800

    Add matlab-emacs integration.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -323,3 +323,11 @@ Also returns nil if pid is nil."
   (load "~/git/quickopen/elisp/quickopen.el"))
 
 (display-battery-mode)
+
+(add-to-list 'load-path "~/.emacs.d/matlab-emacs")
+(load-library "matlab-load")
+(matlab-cedet-setup)
+(add-hook 'matlab-mode
+          (lambda ()
+            (auto-complete-mode 1)
+            ))
