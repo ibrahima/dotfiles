f6152252d9ef99babda8c99bac4bf011ed61e01f
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-06-08 11:21:43 -0700

    Emacs: Function for deleting a buffer and associated file.

--- .emacs.d/my-functions.el
+++ .emacs.d/my-functions.el
@@ -117,3 +117,19 @@
 
 (add-hook 'org-clock-in-hook
           'org-store-current-task)
+
+;; http://tuxicity.se/emacs/elisp/2010/11/16/delete-file-and-buffer-in-emacs.html
+(defun delete-this-buffer-and-file ()
+  "Removes file connected to current buffer and kills buffer."
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (buffer (current-buffer))
+        (name (buffer-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (when (yes-or-no-p "Are you sure you want to remove this file? ")
+        (delete-file filename)
+        (kill-buffer buffer)
+        (message "File '%s' successfully removed" filename)))))
+
+(global-set-key (kbd "C-c k") 'delete-this-buffer-and-file)
