83eedeba0674bacfa883c612ac26bc3058f3bfff
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2015-04-22 14:23:12 -0700

    Add bash functions to run an activity over ADB.

--- .bashrc.d/10-android.sh
+++ .bashrc.d/10-android.sh
@@ -1,5 +1,17 @@
+#!/usr/bin/env bash
+
 export PATH=~/android/sdk/platform-tools:~/android/sdk/tools:$PATH
 export ANDROID_HOME=~/android/sdk
 
 # Use ccache with the Android build system
 export USE_CCACHE=1
+function runactivity() {
+    local activity_name=${2:-MainActivity}
+    adb shell am start -n $1/.$activity_name
+}
+
+function runvnc() {
+    runactivity org.onaips.vnc
+}
+
+alias adb_logfps='adb logcat -v time CALCFPS:D *:S'
