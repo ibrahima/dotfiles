8510b75ce61020048b0e7b2416720f185851e00b
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2018-05-05 22:33:49 -0700

    Adding torch config

---
+++ .bashrc.d/10-torch.sh
@@ -0,0 +1,3 @@
+export PATH=/home/ibrahim/git/torch/install/bin:$PATH  # Added automatically by torch-dist
+export LD_LIBRARY_PATH=/home/ibrahim/git/torch/install/lib:$LD_LIBRARY_PATH  # Added automatically by torch-dist
+export DYLD_LIBRARY_PATH=/home/ibrahim/git/torch/install/lib:$DYLD_LIBRARY_PATH  # Added automatically by torch-dist
