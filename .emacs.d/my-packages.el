ecc0ca1612bc7efd7ee199d887a71724f9e4f03e
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2018-08-01 14:05:14 -0700

    Some melpa thing

--- .emacs.d/my-packages.el
+++ .emacs.d/my-packages.el
@@ -1,7 +1,11 @@
 (require 'package)
 ;;; Code:
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives
