b6551f553ffc89ead9462ea49093fa61fce360fe
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2022-07-25 00:05:33 -0700

    Spring cleaning, switch to chruby
    Been using this for years, not sure why I never updated this.

--- .bashrc.d/10-ruby.sh
+++ .bashrc.d/10-ruby.sh
@@ -1,10 +1,5 @@
-if [ -d ~/.rvm ]; then
-    source ~/.rvm/scripts/rvm
-fi
-
-export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
-export PATH="$HOME/.rbenv/bin:$PATH"
-
-eval "$(rbenv init -)"
+source /usr/local/share/chruby/chruby.sh
+source /usr/local/share/chruby/auto.sh
 
 export RUBY_FFI_NCURSES_LIB=/lib/x86_64-linux-gnu/libncursesw.so.5
+
